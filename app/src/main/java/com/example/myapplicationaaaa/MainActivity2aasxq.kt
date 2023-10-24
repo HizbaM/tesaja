d0adba2e18fbdc4de50ff057bf2f2abf6a9d6696
HizbaM <hizbamatsil.itxpro@gmail.com>
2023-10-24 14:06:34 +0700

    tes 2

---
+++ app/src/main/java/com/example/myapplicationaaaa/MainActivity2aasxq.kt
@@ -0,0 +1,11 @@
+package com.example.myapplicationaaaa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2aasxq : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity2aasxq)
+    }
+}
