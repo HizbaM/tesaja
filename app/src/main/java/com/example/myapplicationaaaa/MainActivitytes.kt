447c0643029cbc9e4d5f784ed1d73d8ea22bc2e0
HizbaM <hizbamatsil.itxpro@gmial.com>
2023-10-24 14:13:37 +0700

    tes 3

---
+++ app/src/main/java/com/example/myapplicationaaaa/MainActivitytes.kt
@@ -0,0 +1,11 @@
+package com.example.myapplicationaaaa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivitytes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activitytes)
+    }
+}
