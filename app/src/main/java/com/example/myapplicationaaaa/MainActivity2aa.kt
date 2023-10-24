6f557ae0475df71e14ee84a9b233533cccc1d7a0
HizbaM <hizbamatsil.itxpro@gmail.com>
2023-10-24 14:02:47 +0700

    tes 1

--- app/src/main/java/com/example/myapplicationaaaa/MainActivity2aa.kt
+++ app/src/main/java/com/example/myapplicationaaaa/MainActivity2aa.kt
@@ -3,11 +3,9 @@ package com.example.myapplicationaaaa
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2aa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //aaaa
+        setContentView(R.layout.activity_main_activity2aa)
     }
 }
